0760109ff522feac393d02db692641ce6bb68fd6
Mobinatorabiyan <m01303007@gmail.com>
2025-04-01 23:03:01 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1, a2, a3, s1, s2;
+            a1 = int.Parse(Console.ReadLine());
+            a2 = int.Parse(Console.ReadLine());
+            a3 = int.Parse(Console.ReadLine());
+            s1 = a3 - a2;
+            s2 = a2 - a1;
+            if (s1 == s2)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("NO");
+            Console.ReadLine();
+        }
+    }
+}
